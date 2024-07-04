cc2540ea5003e048d2e5896e18db7db6bb26f790
Farhad <Farhadrahimiklie786@gmail.com>
2024-07-04 16:36:35 +0430

    Netflix Caousel Slider

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Netflex from './Components/Netflex'
+
+const App = () => {
+ 
+  return (
+    <div>
+      <Navbar/>
+      <Netflex/>
+    </div>
+  )
+}
+
+export default App
+
